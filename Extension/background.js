162b5e172e9b792c5493dac56fa67e9d00d61625
Auraxium <clutchmasterman@gmail.com>
2026-02-08 11:51:35 -0800

    long json

---
+++ Extension/background.js
@@ -0,0 +1,44 @@
+
+let msgs = {
+  streamer: async (e, sendResponse) => {
+    fetch(`http://localhost:3124/streamer?id=${e.id}`).then(res => res.json()).then(res => {
+      console.log(res);
+      chrome.tabs.sendMessage({'streamer': res})
+    }).catch();
+  }
+};
+
+if (chrome?.runtime?.onMessage) {
+  chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    console.log(request)
+    msgs[request.port] && msgs[request.port](request, sendResponse);
+    
+    return true;
+  });
+}
+
+
+function main() {
+
+  // changes_plat = {device: cred.device, }
+  console.log('hello from background')
+  
+}
+
+
+main();
+
+// chrome.windows.onRemoved.addListener(hardSave);
+
+// let background = chrome.extension.getBackgroundPage();
+// background.addEventListener("unload", () => {
+//   cache['baj'] = 'bajbaj'
+//   save({cache})
+// })
+
+// console.log('am i like restarting?');
+
+// add recent stuff with tab.url
+// return chrome.tabs.sendMessage(activeInfo.tabId, { type: "username" }, res => {
+//   last_streamer = res;
+// });
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+//01G3WEGZN0000ET2J0MQP5YJ0G
+//cdn.7tv.app/emote/01G3WEGZN0000ET2J0MQP5YJ0G/1x.webp 1x, //cdn.7tv.app/emote/01FFRN3K3R0009CAK0J1469HYB/2x.webp 2x, //cdn.7tv.app/emote/01FFRN3K3R0009CAK0J1469HYB/4x.webp 4x
+
+function App() {
+
+  return (
+    <div className=" bg-[#2a9c7a]">
+      
+    </div>
+  );
+}
+
+export default App;
